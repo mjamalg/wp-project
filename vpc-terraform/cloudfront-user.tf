81da08e86f1b248e4c1a849890b41d1de0dceb59
Chrysalient, LLC <mjamal.green@gmail.com>
2024-09-28 04:48:41 -0400

    Update cloudfront-user.tf
    Signed-off-by: Chrysalient, LLC <mjamal.green@gmail.com>

--- vpc-terraform/cloudfront-user.tf
+++ vpc-terraform/cloudfront-user.tf
@@ -9,5 +9,8 @@ module "cloudfront-wp-user" {
 
   create_iam_user_login_profile = false
   create_iam_access_key         = false #we create them in eks-ansible/roles/bitnami-wordpress-install
-  policy_arns                   = [ "arn:aws:iam::aws:policy/CloudFrontFullAccess" ]
+  policy_arns                   = [ 
+                              "arn:aws:iam::aws:policy/CloudFrontFullAccess",
+                              "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+                              ]
 }
